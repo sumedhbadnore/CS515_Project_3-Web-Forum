c21360200b7cc4ba53889884939fd6de54d9d39a
Sumedh Badnore <66485793+sumedhbadnore@users.noreply.github.com>
2023-12-12 21:34:07 -0500

    Added post method

--- app.py
+++ app.py
@@ -1,25 +1,44 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from datetime import datetime
+import secrets
 
 print("Server running!")
 app = Flask(__name__)
 
+assign_id = 0
 all_posts = {}
 
 @app.route('/post', methods = ['POST'])
 def new_post():
-    temp_id = 1
-    temp_key = "dkjfe234"
-    post = {
-        'id': temp_id,
-        'key': temp_key,
-        'timestamp': datetime.utcnow().isoformat(),
-        'msg': "Hello World!"
-    }
+    global assign_id, all_posts
+    file_type = request.headers.get('Content-Type')
 
-    all_posts[0] = post
-    return jsonify({
-        'id': temp_id,
-        'key': temp_key,
-        'timestamp': post['timestamp']
-    })
+    if (file_type == 'application/json'):
+        input_data = request.get_json()
+
+        if not 'msg' in input_data:
+            return jsonify({'err': 'Missing msg field'}), 400
+        else:
+            input_msg = input_data['msg']
+
+            post_id = assign_id
+            assign_id += 1
+            post_key = secrets.token_hex(16)
+            post = {
+                'id': post_id,
+                'key': post_key,
+                'timestamp': datetime.utcnow().isoformat(),
+                'msg': input_msg
+            }
+            print(input_msg)     # Testing purpose
+            print(post_key)      # Testing purpose   
+            all_posts[post_id] = post
+
+            return jsonify({
+                'id': post_id,
+                'key': post_key,
+                'timestamp': post['timestamp']
+            })
+
+    else:
+        return jsonify({'err': 'Bad request'}), 400
