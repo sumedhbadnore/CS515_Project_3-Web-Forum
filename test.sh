047b602b61ee60a3ba46de3d341bd0cd20d40faa
Rahul Sohandani <rahulsohandani@Rahuls-MacBook-Air.local>
2023-12-17 18:54:36 -0500

    final commit

--- test.sh
+++ test.sh
@@ -6,5 +6,7 @@ trap 'kill $PID' EXIT # kill the server on exit
 ./run.sh &
 PID=$! # record the PID
 
-# newman run forum_multiple_posts.postman_collection.json -e env.json # use the env file
-newman run Test.postman_collection.json -n 50 # 50 iterations
+newman run forum_multiple_posts.postman_collection.json -e env.json
+newman run forum_post_read_delete.postman_collection.json 
+newman run forum_sample_tests.postman_collection.json -e env.json
+newman run Test.postman_collection.json 
