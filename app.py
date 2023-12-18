7efd026d7b001438f90f76adec89135679b4feef
Rahul Sohandani <rahulsohandani@Rahuls-MacBook-Air.local>
2023-12-17 19:17:20 -0500

    final commit

--- app.py
+++ app.py
@@ -159,8 +159,8 @@ def search_date_time():
         end_datetime_str = request.args.get('end_datetime')
 
         try:
-            start_datetime = datetime.strptime(start_datetime_str, "%Y-%m-%dT%H:%M:%S.%f%z") if start_datetime_str else None
-            end_datetime = datetime.strptime(end_datetime_str, "%Y-%m-%d %H:%M:%S") if end_datetime_str else None
+            start_datetime = datetime.strptime(start_datetime_str, "%Y-%m-%d %H:%M:%S. %f%z") if start_datetime_str else None
+            end_datetime = datetime.strptime(end_datetime_str, "%Y-%m-%d %H:%M:%S. %f%z") if end_datetime_str else None
 
             filtered_posts = []
 
